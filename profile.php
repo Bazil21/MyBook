a76590807f642187e69f2050838604089cc137eb
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2021-02-04 22:37:16 +0500

    today work

---
+++ profile.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+    <title>Profile | MyBook</title>
+    </head>
+    <style>
+    	#blue_bar{
+    		height: 50px;
+    		background-color: #405d9b;
+    		color: #d9dfeb;
+    	}
+    	#search_box{
+    		width: 400px;
+    		height: 30px;
+    		border-radius: 5px;
+    		border: none;
+    		padding: 4px;
+    		font-size: 14px;
+    		background-image: url(search.png);
+    		background-repeat: no-repeat;
+    		background-position: right;
+    	}
+    	#profile_pics{
+    		width: 150px;
+    		margin-top: -140px;
+    		border-radius: 50%;
+    		border: solid 2px white;
+    	}
+    </style>
+
+    <body style="font-family: tahoma; background-color: #d0d8e4;">
+
+    	<!-- top bar -->
+    	<br>
+    <div id="blue_bar">
+ 		<div style="width: 800px; margin: auto; font-size: 30px;">
+ 			MyBook &nbsp &nbsp <input type="text" id="search_box" placeholder="Search for people">
+ 			<img src="selfie.jpg" style="width: 50px; float: right;">
+ 		</div>
+    </div>
+
+    <!-- cover area -->
+    <br>
+    <div style="width: 800px; margin: auto; background-color: black; min-height: 400px;">
+
+    	<div style="background-color: white; text-align: center; color: #405d9b;">
+    		<img src="mountain.jpg" width="100%">
+    		<img id="profile_pics" src="selfie.jpg"><br>
+    		Mary Banda
+    		<br><br>
+    		Timeline About Friends Photos Settings
+    	</div>
+
+
+    </div>
+
+
+
+    </body>
+
+</html>
